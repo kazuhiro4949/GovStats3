60a39ad9bf8aec89e90350e394f6d464e157a6eb
Kazuhiro Hayashi <kazuhiro.hayashi.49@gmail.com>
2022-06-26 22:24:36 +0900

    first commit

---
+++ GovStats3/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  GovStats3
+//
+//  Created by kazuhiro2 on 2022/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GovStats3/GovStats3App.swift
@@ -0,0 +1,17 @@
+//
+//  GovStats3App.swift
+//  GovStats3
+//
+//  Created by kazuhiro2 on 2022/06/26.
+//
+
+import SwiftUI
+
+@main
+struct GovStats3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
